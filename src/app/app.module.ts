bf9d0b313b3d43ce62614bb25d106e5f961fc202
Igor <igor.borowski@capgemini.com>
2022-06-23 18:00:58 +0200

    clean app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { HeaderComponent } from './shared/layouts/header/header.component';
-import { FooterComponent } from './shared/layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
